54eb3b8eda9462c3524da21c19b42e25d18b34de
Joseph7Cartaphilus <artem.harov.120@mail.ru>
2024-08-24 16:02:51 +0300

    + док стринги

--- conftest.py
+++ conftest.py
@@ -4,11 +4,17 @@ from selenium import webdriver
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
+    """
+    Фиксирует директорию для генерации отчётов Allure
+    """
     config.option.allure_report_dir = "reports"
 
 
 @pytest.fixture
 def setup():
+    """
+    Настройка фикстуры для тестов
+    """
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(10)
